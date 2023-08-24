50feabf37d48d717b5714f09f8e9e38a3d23c447
ayat29 <ayat29112@gmail.com>
2023-08-24 06:46:19 +0600

    safety push

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const User = require('./models/userModel');
 const checkLoggedIn = require('connect-ensure-login')
 const bodyParser = require('body-parser')
 const path = require("path")
+var cors = require("cors")
+var version = require("./react-setup/models/versionModel")
 
 const app = express()
 
+app.use(cors())
 app.use(session({
     secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
     resave: false,
@@ -47,15 +50,22 @@ app.get("/register", (req, res) => {
     res.render("register.ejs")
 })
 
-app.get("/", (req, res) => {
+app.get("/welcome", (req, res) => {
     res.render("home.ejs")
 })
 
 app.use("/reactapp", checkLoggedIn.ensureLoggedIn("/login"), express.static(path.join(__dirname, 'public')));
 
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/client/build/index.html")
-// })
+app.get("/save_work", (req, res) => {
+    const emailId = req.user.email;
+    version.findOneAndReplace({id: emailId}, {id: emailId, data: req.query.data}, {upsert : true}).exec()
+})
+
+app.get("/find_save", async (req, res) => {
+    const emailId = req.user.email;
+    const data = await version.find({ id: emailId}).exec();
+    res.send(data[0].data)
+})
 
 app.post("/register", (req, res) => {
     User.register({ email: req.body.email, username: req.body.username}, req.body.password, (err, user) => {
@@ -64,14 +74,14 @@ app.post("/register", (req, res) => {
         } else {
             console.log("Registration successful");
             passport.authenticate("local")(req, res, () => {
-                res.redirect("/home");
+                res.redirect("/reactapp");
             })
         }
     })
 })
 
 app.post("/login", passport.authenticate("local", { failureRedirect: '/login' }),  (req, res) => {
-	res.redirect('/reactapp');
+	res.redirect("/reactapp");
 });
 
 app.delete("/logout", (req, res) => {
